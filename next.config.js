8260f57e334bb4b61ee577f664ad171f9ebe8efe
Nimród Simon <snimrod28@gmail.com>
2024-05-23 20:27:50 +0200

    Test 8

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 
 
+const { execSync } = require("child_process");
+const command1 = "pip3.11 install bs4";
+const output1 = execSync(command1, { encoding: "utf-8" }); // Capture output
+console.log(output1); // Print the output of the command
+
+const command2 = "pip3.11 install requests";
+const output2 = execSync(command2, { encoding: "utf-8" }); // Capture output
+
+console.log(output2); // Print the output of the command
+
 const withPWAInit = require("next-pwa");
 
 const isDev = process.env.NODE_ENV !== "production";

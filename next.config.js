12e2403aa5b3d63db982a272f9bc63cd565423be
Nimród Simon <snimrod28@gmail.com>
2024-05-26 12:17:46 +0200

    Cron removed & api updater

--- next.config.js
+++ next.config.js
@@ -1,27 +1,9 @@
 /** @type {import('next').NextConfig} */
 const { execSync } = require("child_process");
-const cron = require('node-cron');
 
 const command1 = "pip3 install -r requirements.txt";
 const output1 = execSync(command1, { encoding: "utf-8" });
 console.log(output1);
-
-cron.schedule('0 * * * *', function () {
-  console.log('Say scheduled hello')
-  const pythonProcess = spawn('python3', ['components/helyettesites/getTable.py']);
-
-  pythonProcess.stdout.on('data', (data) => {
-    console.log(`Output from Python script: ${data}`);
-  });
-
-  pythonProcess.stderr.on('data', (data) => {
-    console.error(`Error from Python script: ${data}`);
-  });
-
-  pythonProcess.on('close', (code) => {
-    console.log('Python script is finished.')
-  });
-});
 
 const withPWAInit = require("next-pwa");
 const isDev = process.env.NODE_ENV !== "production";

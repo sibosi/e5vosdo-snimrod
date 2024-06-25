91b9335408d02b14f58f33c9465fab511bf7ce96
Nimród Simon <snimrod28@gmail.com>
2024-06-25 13:41:05 +0200

    add fake auth

--- auth.ts
+++ auth.ts
@@ -1,9 +1,41 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const {
+let auth: any;
+
+if (process.env.FAKE_AUTH == "true") {
+  auth = () => {
+    return {
+      user: {
+        name: process.env.FAKE_NAME,
+        email: process.env.FAKE_EMAIL,
+        image: process.env.FAKE_IMAGE,
+      },
+    };
+  };
+} else {
+  auth = NextAuth({
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+
+        authorization: {
+          params: {
+            prompt: "consent",
+            access_type: "offline",
+            response_type: "code",
+          },
+        },
+      }),
+    ],
+    secret: process.env.AUTH_SECRET,
+    basePath: "/api/auth",
+  });
+}
+
+const {
   handlers: { GET, POST },
-  auth,
   signIn,
   signOut,
 } = NextAuth({
@@ -24,3 +56,5 @@ export const {
   secret: process.env.AUTH_SECRET,
   basePath: "/api/auth",
 });
+
+export { GET, POST, auth, signIn, signOut };

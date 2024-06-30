da5f2fa4fe2a3acc243997fe424a93436d644e0c
Nimród Simon <snimrod28@gmail.com>
2024-06-30 22:47:43 +0200

    One sw is enough

--- components/PWA/serviceWorker.tsx
+++ components/PWA/serviceWorker.tsx
@@ -19,17 +19,6 @@ function ServiceWorker() {
             console.log("ServiceWorker registration failed: ", error);
           }
         );
-        navigator.serviceWorker.register("/sw.js").then(
-          (registration) => {
-            console.log(
-              "ServiceWorker registration successful with scope: ",
-              registration.scope
-            );
-          },
-          (error) => {
-            console.log("ServiceWorker registration failed: ", error);
-          }
-        );
       });
 
       navigator.serviceWorker.addEventListener("push", async function (event) {

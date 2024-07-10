9841bc1f0ff9b6f4dbd497bd45c19c26c99448a0
Nimród Simon <snimrod28@gmail.com>
2024-07-10 21:40:20 +0200

    feat: Add timetable management system
    The commit adds a new file `main.py` which contains the initial implementation of a timetable management system for a school. The system allows the admin to manage classes, students, teachers, and the timetable itself. It also includes other features that are yet to be implemented.

---
+++ timetable/main.py
@@ -0,0 +1,11 @@
+# This project is a timetable management system for a school. It is a simple project that allows the admin to manage the timetable of the school. The admin can add, update, and delete timetable details. It does not have UI. This program will be translated into typescript and react in the future.
+# The program has the following features:
+# 1. Manage classes
+# 2. Manage students
+# 3. Manage teachers
+# 4. Manage timetable
+# 5. Others...
+
+'''
+id, name, description, slot_id, location_id, signup_type, is_competition, signup_dealine, organiser, capacity, img_url, starts_at, ends_at, deleted_at, created_at, updated_at, root_parent, direct_child, min_team_size, max_team_size
+'''

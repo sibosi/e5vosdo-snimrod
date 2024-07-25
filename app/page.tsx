977f65a158c6b3aa0d36a0ab0f047b4301734635
Nimród Simon <snimrod28@gmail.com>
2024-07-25 14:02:49 +0200

    Sm change

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default async function Home() {
         )}
       </div>
 
-      <TimetableDay selfUser={selfUser} />
+      {selfUser ? <TimetableDay selfUser={selfUser} /> : <></>}
 
       {siteConfig.pageSections["teremcserek"] != "hidden" && (
         <Section

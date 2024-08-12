d819b2a8c4419c6e9135ec63f79fd8ac197d3aa7
Nimród Simon <snimrod28@gmail.com>
2024-08-12 13:27:00 +0200

    skipMessenger pre test081201

--- app/skipMessenger.tsx
+++ app/skipMessenger.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Modal, ModalContent, ModalHeader } from "@nextui-org/react";
+import { Button, Modal, ModalContent, ModalHeader } from "@nextui-org/react";
 import React, { useEffect, useState } from "react";
 
 const SkipMessenger: React.FC = () => {
@@ -22,13 +22,13 @@ const SkipMessenger: React.FC = () => {
     if (isMessengerBrowser) {
       // Átirányítás Chrome-ba, vagy Safari-ra (iOS)
       const chromeUrl = `googlechrome://navigate?url=${window.location.href}`;
-      const safariUrl = `https://www.google.com/chrome/`;
+      const safariUrl = `https://info.e5vosdo.hu/`;
 
       window.location.href = chromeUrl;
 
       setTimeout(() => {
-        window.location.href = safariUrl;
-      }, 500); // 500ms várakozás után átirányítás Safari-ra, ha a Chrome nem található
+        // window.location.href = safariUrl;
+      }, 50000); // 500ms várakozás után átirányítás Safari-ra, ha a Chrome nem található
     }
   }, [isMessengerBrowser]);
 
@@ -42,6 +42,14 @@ const SkipMessenger: React.FC = () => {
               Az oldal megtekintéséhez használja a Google Chrome böngészőt vagy
               a Safari-t (iOS).
             </p>
+            <p className="tegt-xl text-primary font-bold">info.e5vosdo.hu</p>
+            <Button
+              onClick={() => {
+                navigator.clipboard.writeText("info.e5vosdo.hu");
+              }}
+            >
+              Cím másolása
+            </Button>
           </ModalContent>
         </Modal>
       ) : null}

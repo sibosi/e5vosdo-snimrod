78d52983c36ef738b4de14f04d019d619fd548e1
Nimród Simon <snimrod28@gmail.com>
2024-08-12 13:37:35 +0200

    Bugfix

--- components/navbar/profilebox/notificationBox.tsx
+++ components/navbar/profilebox/notificationBox.tsx
@@ -9,6 +9,7 @@ export const NotificationBox = ({
   time,
   onClick,
   className,
+  color,
 }: {
   icon: React.ReactNode;
   title: string;
@@ -17,11 +18,33 @@ export const NotificationBox = ({
   time?: string;
   onClick?: () => void;
   className?: string;
+  color?:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "warning"
+    | "error"
+    | "foreground";
 }) => {
+  const colorStyle = color
+    ? {
+        primary: "bg-primary-100 text-primary-700",
+        secondary: "bg-secondary-100 text-secondary-700",
+        success: "bg-success-100 text-success-700",
+        warning: "bg-warning-100 text-warning-700",
+        error: "bg-error-100 text-error-700",
+        foreground: "bg-foreground-100 text-foreground-700",
+      }[color]
+    : "";
+
   return (
-    <div className={"px-1 rounded-2xl " + className}>
+    <div className={"px-1 rounded-2xl " + className + " " + colorStyle}>
       <div className="flex my-3 gap-2" onClick={onClick}>
-        <div className={"block w-5 h-5 m-1 my-auto " + (className ?? "")}>
+        <div
+          className={
+            "block w-5 h-5 m-1 my-auto " + (className ?? "") + " " + colorStyle
+          }
+        >
           {icon}
         </div>
         <div className="text-left truncate w-full">

2435895603fb861712c3c085bf5a6bcfc472b7f7
Nimród Simon <snimrod28@gmail.com>
2024-08-20 21:23:03 +0200

    Offline mode chapter 1

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -26,3 +26,64 @@ self.addEventListener("notificationclick", function (event) {
       }),
   );
 });
+
+const CACHE_NAME = 'cache ' + Date.now();
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/clubs',
+
+        '/favicon.ico',
+        '/bimun.jpg',
+        '/debate.png',
+        '/diak.jpg',
+        '/eam.png',
+        '/elsosegely.jpg',
+        '/kektura.jpg',
+        '/kosar.jpg',
+        '/media.jpg',
+        '/munclub.png',
+        '/nekunkx.png',
+        '/sakk.jpg',
+        '/suliradio.png',
+        '/szinjatszo.png',
+        '/tarsastar.png',
+        '/technikusi.png',
+        '/techteam.jpg',
+        '/zoldbiz.png',
+
+        // További statikus erőforrások hozzáadása itt
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((networkResponse) => {
+        return caches.open('my-app-cache').then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
+});

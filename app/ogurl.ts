37033325fc163433f5317df31ea0811134ef91fd
Nimród Simon <snimrod28@gmail.com>
2024-08-24 21:21:47 +0200

    Test 082401

--- app/ogurl.ts
+++ app/ogurl.ts
@@ -1,9 +1,11 @@
+import { redirect } from "next/navigation";
+
 export default function OGURL() {
   if (typeof window === "undefined") return;
   const allowedDomains = ["info.e5vosdo.hu"];
   const currentDomain = window.location.hostname;
 
   if (!allowedDomains.includes(currentDomain)) {
-    window.location.href = "https://info.e5vosdo.hu" + window.location.pathname;
+    redirect("https://info.e5vosdo.hu" + window.location.pathname);
   }
 }

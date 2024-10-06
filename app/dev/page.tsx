0be82e287d1ab0d5e9b9a9d6e67a0fa03e00c731
Nimród Simon <snimrod28@gmail.com>
2024-10-06 21:11:12 +0200

    bugfix

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -1,8 +1,8 @@
 import { redirect } from "next/navigation";
 import { getAuth } from "@/db/dbreq";
-import TimetableWeek from "@/components/timetable/timetableweek";
-import TimetableDay from "@/components/timetable/timetableday";
-import TimetableFromJSON from "./timetableFromJSON";
+// import TimetableWeek from "@/components/timetable/timetableweek";
+// import TimetableDay from "@/components/timetable/timetableday";
+// import TimetableFromJSON from "./timetableFromJSON";
 import FileUploader from "./uploader";
 import EditEvents from "./editEvents";
 // import { exportTimetable } from "@/timetable/export";

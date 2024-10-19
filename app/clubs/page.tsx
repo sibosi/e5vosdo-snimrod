f699c2028539c1d0ca30ec98167cf31f9ef1371e
Nimród Simon <snimrod28@gmail.com>
2024-10-19 11:13:05 +0200

    add ink to clubs

--- app/clubs/page.tsx
+++ app/clubs/page.tsx
@@ -1,6 +1,7 @@
 import { clubsConfig, clubsOrder } from "@/config/groups";
 import PopupCards from "@/components/popupcards";
 import { Alert } from "@/components/home/alert";
+import { Link } from "@nextui-org/react";
 
 function sortClubs() {
   const sortedClubs = new Set();
@@ -19,6 +20,12 @@ export default function ClubsPage() {
       <h1 className="pb-8 text-center text-4xl font-semibold text-foreground lg:text-5xl">
         Klubok és szakkörök
       </h1>
+
+      <Alert className="border-selfprimary-300 bg-selfprimary-100">
+        <Link href="https://map.barnagoz.com/">
+          Interaktív térképen is megtalálod a klubokat! (Itt)
+        </Link>{" "}
+      </Alert>
 
       <Alert className="border-selfprimary-300 bg-selfprimary-100">
         A kártyákra kattintva további információkat találsz a klubokról.

3cc0dbcff308008b2fd0693e8117de5e4e515bab
Nimród Simon <snimrod28@gmail.com>
2024-10-22 01:37:51 +0200

    make /alerts api dynamic

--- app/api/alerts/route.ts
+++ app/api/alerts/route.ts
@@ -1,7 +1,8 @@
-import { getAlerts } from "@/db/dbreq";
+import { getAlerts, getAuth } from "@/db/dbreq";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  const selfUser = getAuth()
   const data = await getAlerts();
 
   return NextResponse.json(data);

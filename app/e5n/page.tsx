1e2762138db4d439c9edb4e157cfd20739a8ed10
Nimród Simon <snimrod28@gmail.com>
2024-10-25 23:32:54 +0200

    Say bye

--- app/e5n/page.tsx
+++ app/e5n/page.tsx
@@ -7,6 +7,14 @@ import teachers from "./teachers.json";
 
 const E5NPage = async () => {
   const selfUser = await getAuth();
+  return (
+    <div className="fixed left-0 top-0 z-0 flex h-full w-full flex-col justify-center bg-background bg-opacity-90 px-5">
+      <h1 className="mb-2 text-center text-3xl font-semibold text-foreground">
+        Ezt a felületet felejtsük el. Elnézést a kellemetlenségekért - a
+        fejlesztő
+      </h1>
+    </div>
+  );
   if (!selfUser)
     return (
       <div className="font-semibold text-foreground">

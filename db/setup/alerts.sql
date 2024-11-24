6f6fffa7954df09761b3fef28d7a0bdea387cb20
Nimród Simon <snimrod28@gmail.com>
2024-11-24 22:27:23 +0100

    Update db

--- db/setup/alerts.sql
+++ db/setup/alerts.sql
@@ -9,5 +9,4 @@ CREATE TABLE IF NOT EXISTS alerts (
 INSERT INTO alerts (text, className, padding, icon)
 VALUES 
 ("r_edirect=https://e5vosdo.hu", "bg-selfprimary-300 border-selfprimary-400 mx-auto hidden", 1, 0),
-("<a href='https://docs.google.com/spreadsheets/d/1_-GniTd1e6O1rpxLq3qCz1g2YUijh8Z1Lt_ejF8Y6nY/edit?gid=128007900#gid=128007900' className='text-selfsecondary-700'>Aktuális teremcserék itt</a>", "bg-selfprimary-300 border-selfprimary-400 mx-auto text-md", 1, 0),
 ("Saját előadások megtekintése: <a href='https://info.e5vosdo.hu/e5n' className='text-selfsecondary-700'>info.e5vosdo.hu/e5n</a>", "bg-selfprimary-300 border-selfprimary-400 mx-auto text-xl hidden", 1, 0);

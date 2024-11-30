1b78d52cc5b731cb12e0c9798f7606e8dd7864e3
Nimród Simon <snimrod28@gmail.com>
2024-11-30 21:32:57 +0100

    Remove daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,35 +12,35 @@ module.exports = {
     extend: {
       colors: {
         selfprimary: {
-          DEFAULT: 'var(--color-primary)',
-          "bg": 'var(--color-primary-bg)',
-          0: 'var(--color-primary-0)',
-          20: 'var(--color-primary-20)',
-          50: 'var(--color-primary-50)',
-          100: 'var(--color-primary-100)',
-          200: 'var(--color-primary-200)',
-          300: 'var(--color-primary-300)',
-          400: 'var(--color-primary-400)',
-          500: 'var(--color-primary-500)',
-          600: 'var(--color-primary-600)',
-          700: 'var(--color-primary-700)',
-          800: 'var(--color-primary-800)',
-          900: 'var(--color-primary-900)',
+          DEFAULT: "var(--color-primary)",
+          bg: "var(--color-primary-bg)",
+          0: "var(--color-primary-0)",
+          20: "var(--color-primary-20)",
+          50: "var(--color-primary-50)",
+          100: "var(--color-primary-100)",
+          200: "var(--color-primary-200)",
+          300: "var(--color-primary-300)",
+          400: "var(--color-primary-400)",
+          500: "var(--color-primary-500)",
+          600: "var(--color-primary-600)",
+          700: "var(--color-primary-700)",
+          800: "var(--color-primary-800)",
+          900: "var(--color-primary-900)",
         },
         selfsecondary: {
-          DEFAULT: 'var(--color-secondary)',
-          0: 'var(--color-secondary-0)',
-          20: 'var(--color-secondary-20)',
-          50: 'var(--color-secondary-50)',
-          100: 'var(--color-secondary-100)',
-          200: 'var(--color-secondary-200)',
-          300: 'var(--color-secondary-300)',
-          400: 'var(--color-secondary-400)',
-          500: 'var(--color-secondary-500)',
-          600: 'var(--color-secondary-600)',
-          700: 'var(--color-secondary-700)',
-          800: 'var(--color-secondary-800)',
-          900: 'var(--color-secondary-900)',
+          DEFAULT: "var(--color-secondary)",
+          0: "var(--color-secondary-0)",
+          20: "var(--color-secondary-20)",
+          50: "var(--color-secondary-50)",
+          100: "var(--color-secondary-100)",
+          200: "var(--color-secondary-200)",
+          300: "var(--color-secondary-300)",
+          400: "var(--color-secondary-400)",
+          500: "var(--color-secondary-500)",
+          600: "var(--color-secondary-600)",
+          700: "var(--color-secondary-700)",
+          800: "var(--color-secondary-800)",
+          900: "var(--color-secondary-900)",
         },
       },
     },
@@ -50,16 +50,6 @@ module.exports = {
     },
   }, // Add your tailwind config here
   darkMode: "class",
-  plugins: [nextui(), require("daisyui")],
-  require: [("daisyui")],
-  daisyui: {
-    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
-  },
+  plugins: [nextui()],
+  require: [],
 };

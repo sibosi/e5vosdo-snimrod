0a828c164056d7e50148a534c2f41ef4870fd664
Nimród Simon <snimrod28@gmail.com>
2024-12-08 16:51:47 +0100

    delete duplicate redirect code

--- app/ogurl.tsx
+++ app/ogurl.tsx
@@ -5,15 +5,16 @@ import { useRouter } from "next/navigation";
 
 export default function OGURL() {
   const router = useRouter();
+  const isFakeAuth = process.env.FAKE_AUTH === "true";
 
   useEffect(() => {
-    const allowedDomains = ["info.e5vosdo.hu", "localhost"];
+    const allowedDomains = ["info.e5vosdo.hu", "localhost", "192."];
     const currentDomain = window.location.hostname;
 
-    if (!allowedDomains.includes(currentDomain)) {
+    if (!allowedDomains.includes(currentDomain) && !isFakeAuth) {
       router.push("https://info.e5vosdo.hu" + window.location.pathname);
     }
-  }, [router]);
+  }, [isFakeAuth, router]);
 
   return null;
 }

d8fc1633bccdfd23229f43571543edf44f3d53ff
Nimród Simon <81036480+sibosi@users.noreply.github.com>
2024-12-09 15:24:43 +0000

    fix: update allowed domains for URL redirection

--- app/ogurl.tsx
+++ app/ogurl.tsx
@@ -7,10 +7,13 @@ export default function OGURL({ isFakeAuth }: { isFakeAuth: boolean }) {
   const router = useRouter();
 
   useEffect(() => {
-    const allowedDomains = ["info.e5vosdo.hu", "localhost", "192."];
+    const allowedDomains = ["info.e5vosdo.hu", "localhost", "192.", "github"];
     const currentDomain = window.location.hostname;
 
-    if (!allowedDomains.includes(currentDomain) && !isFakeAuth) {
+    const isDomainAllowed = allowedDomains.some((domain) =>
+      currentDomain.includes(domain),
+    );
+    if (!isDomainAllowed && !isFakeAuth) {
       router.push("https://info.e5vosdo.hu" + window.location.pathname);
     }
   }, [isFakeAuth, router]);

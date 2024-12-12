7265c853517e6ba67b5353ad945eafdb6c1381ba
Nimród Simon <snimrod28@gmail.com>
2024-12-12 21:40:45 +0100

    fix: update redirect URL for social login to use APP_DOMAIN

--- actions/route.ts
+++ actions/route.ts
@@ -4,7 +4,7 @@ import { signIn, signOut } from "@/auth";
 
 export async function doSocialLogin(formData: any) {
   const action = formData.get("action");
-  await signIn(action, { redirectTo: "/" });
+  await signIn(action, { redirectTo: process.env.APP_DOMAIN });
   console.log("Action");
 }
 

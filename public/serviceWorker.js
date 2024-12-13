96807de7ef35325d26ccfed1466d8f0023913fe7
Nimród Simon <snimrod28@gmail.com>
2024-12-14 00:51:21 +0100

    make sw simpler

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,8 +1,6 @@
 "use strict";
 
-const SW_settings_name = "SW-settings";
-
-const CACHE_NAME_BEGINNING = "simple-cache-";
+const CACHE_NAME_BEGINNING = "simple-cache";
 const CACHE_NAME = async () => {
   return (
     CACHE_NAME_BEGINNING +
@@ -55,9 +53,9 @@ self.addEventListener("notificationclick", function (event) {
       .then(function (clientList) {
         if (clientList.length > 0) {
           let client = clientList[0];
-          for (let i = 0; i < clientList.length; i++) {
-            if (clientList[i].focused) {
-              client = clientList[i];
+          for (const clientItem of clientList) {
+            if (clientItem.focused) {
+              client = clientItem;
             }
           }
           return client.focus();
@@ -79,9 +77,6 @@ const DISALLOWED_URLS = [
 
 const DISALLOWED_URL_BEGINNINGS = ["/api"];
 
-let cacheMethod = null;
-(async () => (cacheMethod = await getStorage("cacheMethod")))();
-
 self.addEventListener("fetch", (event) => {
   if (event.request.destination === "image")
     event.respondWith(
@@ -89,9 +84,9 @@ self.addEventListener("fetch", (event) => {
         if (response) return response;
 
         return fetch(event.request).then((response) => {
+          const responseToCache = response.clone();
           caches.open("images").then((cache) => {
-            const expirationTime = Date.now() + 1000 * 60 * 60 * 24 * 10; // 10 day
-            const responseToCache = response.clone();
+            const expirationTime = Date.now() + 1000 * 60 * 60 * 24 * 10; // 10 nap
             responseToCache.headers.append(
               "sw-expiration-time",
               expirationTime,
@@ -105,100 +100,8 @@ self.addEventListener("fetch", (event) => {
     );
 });
 
-self.addEventListener("nofetch", (event) => {
-  checkForUpdate();
-  if (event.request.method !== "GET") return;
-
-  const shouldDisallow = DISALLOWED_URLS.some((disallowedUrl) => {
-    return event.request.url.includes(disallowedUrl);
-  });
-
-  const shouldDisallowBeginning = DISALLOWED_URL_BEGINNINGS.some(
-    (disallowedUrlBeginning) => {
-      return event.request.url.startsWith(disallowedUrlBeginning);
-    },
-  );
-
-  if (shouldDisallow || shouldDisallowBeginning) {
-    return;
-  }
-
-  event.respondWith(
-    (async () => {
-      const url = new URL(event.request.url);
-
-      if (url.pathname === "/clearCache")
-        caches.keys().then((cacheNames) => {
-          cacheNames.forEach((cacheName) => caches.delete(cacheName));
-        });
-
-      if (cacheMethod === null) {
-        cacheMethod = await getStorage("cacheMethod");
-        console.log("Chachce updated:", cacheMethod);
-      }
-
-      if (cacheMethod === "never") return fetch(event.request);
-      if (cacheMethod === "offline" && navigator.onLine)
-        return fetch(event.request);
-      if (url.pathname.startsWith("/_next/image")) {
-        const cache = await caches.open("dynamic-images-cache");
-        const cachedResponse = await cache.match(event.request);
-
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        try {
-          const networkResponse = await fetch(event.request);
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        } catch (error) {
-          console.error("Failed to fetch and cache image:", error);
-          throw error;
-        }
-      } else {
-        const cache = await caches.open(await CACHE_NAME());
-        const cachedResponse = await cache.match(event.request);
-
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        try {
-          const networkResponse = await fetch(event.request);
-
-          const shouldDisallow = DISALLOWED_URLS.some((disallowedUrl) => {
-            return event.request.url.includes(disallowedUrl);
-          });
-
-          const shouldDisallowBeginning = DISALLOWED_URL_BEGINNINGS.some(
-            (disallowedUrlBeginning) => {
-              return event.request.url.startsWith(disallowedUrlBeginning);
-            },
-          );
-
-          if (shouldDisallow || shouldDisallowBeginning) {
-            return networkResponse;
-          }
-
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        } catch (error) {
-          console.error("Fetch failed:", error);
-          throw error;
-        }
-      }
-    })(),
-  );
-});
-
 self.addEventListener("message", (event) => {
-  if (event.data && event.data.action === "cacheMethodUpdated") {
-    (async () => (cacheMethod = await getStorage("cacheMethod")))();
-  }
   if (event.data && event.data.action === "reCache") {
-    // Delete all cache
-
     event.waitUntil(
       caches.keys().then((cacheNames) => {
         return Promise.all(
@@ -208,7 +111,5 @@ self.addEventListener("message", (event) => {
         );
       }),
     );
-
-    return;
   }
 });

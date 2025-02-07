263bae6e27088084c366d6bd9a80ecc1ea7e6c1b
Nimród Simon <snimrod28@gmail.com>
2025-02-07 22:55:07 +0100

    nothing 2

--- app/api/backup/route.ts
+++ app/api/backup/route.ts
@@ -3,5 +3,5 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const data = await backup();
-  return NextResponse.json(data);
+  return NextResponse.json({ data });
 }

075d2a398e771a92dff4b4bd8067143d54641849
Nimród Simon <snimrod28@gmail.com>
2025-02-19 21:00:47 +0100

    forget e5n pt.2

--- app/api/getMyPre/route.ts
+++
@@ -1,8 +0,0 @@
-import { getMyPre } from "@/db/dbreq";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest) {
-  const response = await getMyPre();
-
-  return NextResponse.json(response, { status: 200 });
-}
--- app/api/presentations/route.ts
+++
@@ -1,9 +0,0 @@
-import { getAuth, getPresentations } from "@/db/dbreq";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const selfUser = getAuth();
-  const data = await getPresentations();
-
-  return NextResponse.json(data);
-}

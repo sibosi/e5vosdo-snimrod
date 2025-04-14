546343638e001637956d0396d02f18da780fa24c
Nimród Simon <81036480+sibosi@users.noreply.github.com>
2025-04-14 14:06:01 +0000

    Bugfix

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default async function Home() {
         return <Elections />;
 
         if (selfUser?.permissions.includes("user")) {
-          return <Carousel selfUser={selfUser} data={[]} />;
+          // return <Carousel selfUser={selfUser} data={[]} />;
         } else if (selfUser === null) {
           return (
             <Tray>

2ae0ce189c01ac99ee9d7c3073710c91ffb51d12
Nimród Simon <81036480+sibosi@users.noreply.github.com>
2025-04-25 19:30:16 +0000

    tmp

--- next.config.js
+++ next.config.js
@@ -62,18 +62,6 @@ const nextConfig = {
     });
     return config;
   },
-  cookies: {
-    sessionToken: {
-      name: `__Secure-next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: process.env.NODE_ENV === "production",
-        domain: ".e5vosdo.hu",
-      },
-    },
-  },  
   serverExternalPackages: ["pdfkit"],
   experimental: {
     serverActions: {

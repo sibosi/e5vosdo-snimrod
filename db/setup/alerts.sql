2ac61be862c1e87216a7d89a8f102d5c547fcc97
Nimród Simon <snimrod28@gmail.com>
2025-05-21 21:22:38 +0200

    fix(alerts): update alert messages and styles for consistency

--- db/setup/alerts.sql
+++ db/setup/alerts.sql
@@ -7,15 +7,10 @@ CREATE TABLE IF NOT EXISTS alerts (
     icon BOOLEAN DEFAULT 0
 );
 INSERT INTO alerts (text, className, padding, icon)
-VALUES (
-        "r_edirect=https://e5vosdo.hu",
-        "bg-selfprimary-300 border-selfprimary-400 mx-auto hidden",
-        1,
-        0
-    ),
+VALUES
     (
-        "A 8-11. évfolyamosok jelentkezési felülete: <a href='https://e5vosdo.hu/jelentkezes' className='text-selfsecondary-700'>e5vosdo.hu/jelentkezes</a>",
-        "bg-selfprimary-100 border-selfprimary-300 mx-5 text-md hidden",
+        "<a href='https://docs.google.com/spreadsheets/d/1097V-LMPhvk4vhOe8B_zUEp2Fpf7mtGs' className='text-selfprimary-700'>Az aktuális teremcseréket itt találod.</a>",
+        "bg-selfprimary-100 border-selfprimary-300 mx-5 text-md",
         1,
         NULL
     );

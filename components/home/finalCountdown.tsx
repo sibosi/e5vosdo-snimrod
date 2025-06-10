0ff7f2b0564c3456b60d516fcc5ebd85ffc12e76
Nimród Simon <snimrod28@gmail.com>
2025-06-10 23:37:31 +0200

    fix: update FinalCountdown component to use correct date for Vakacio and Countdown

--- components/home/finalCountdown.tsx
+++ components/home/finalCountdown.tsx
@@ -5,8 +5,8 @@ import { Vakacio } from "./vakacio";
 const FinalCountdown = () => {
   return (
     <div className="mb-4 flex h-[150px] flex-col items-center justify-center gap-4">
-      <Vakacio date="2025-06-20T13:15:00Z" />
-      <Countdown date="2025-06-20T13:15:00Z" />
+      <Vakacio date="2025-06-20T09:00:00Z" />
+      <Countdown date="2025-06-20T09:00:00Z" />
     </div>
   );
 };

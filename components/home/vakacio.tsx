a5a6c993c90549da499ad60dfef14f49d28630be
Nimród Simon <snimrod28@gmail.com>
2025-06-13 21:42:25 +0200

    fix: refactor Vakacio component to correctly calculate weekdays left until vacation

--- components/home/vakacio.tsx
+++ components/home/vakacio.tsx
@@ -1,18 +1,18 @@
-"use client";
-
 export const Vakacio = ({ date }: { date: string }) => {
-  const timeLeft =
-    Math.floor((+new Date(date) - +new Date()) / (1000 * 60 * 60 * 24)) + 1;
+  const now = new Date();
+  const targetDate = new Date(date);
+  const MILLISECONDS_IN_DAY = 24 * 60 * 60 * 1000;
+  let weekdaysLeft = 0;
 
-  const colors = [
-    ["#39b2f8", "#2747fc"],
-    ["#FF1CF7", "#b249f8"],
-    ["#f5ab61", "#ecf55f"],
-  ];
+  while (now < targetDate) {
+    const dayOfWeek = now.getDay();
+    if (dayOfWeek !== 5 && dayOfWeek !== 6) weekdaysLeft++;
+    now.setTime(now.getTime() + MILLISECONDS_IN_DAY);
+  }
 
   return (
-    <p className="from-selfsecondary-200 to-selfsecondary-500 mx-auto max-w-fit bg-gradient-to-l bg-clip-text pt-2 text-center text-5xl font-bold text-transparent transition-all duration-500 ease-in-out">
-      {"VAKÁC--IÓ!".substring(timeLeft)}
+    <p className="mx-auto max-w-fit bg-gradient-to-l from-selfsecondary-200 to-selfsecondary-500 bg-clip-text pt-2 text-center text-5xl font-bold text-transparent transition-all duration-500 ease-in-out">
+      {"VAKÁCIÓ!".substring(weekdaysLeft)}
     </p>
   );
 };

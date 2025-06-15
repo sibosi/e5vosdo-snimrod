818a83b4f44d327954cf16ef3af9fb547f318a22
Nimród Simon <snimrod28@gmail.com>
2025-06-15 20:44:53 +0200

    release waveText (VAKÁCIÓ!)

--- components/home/finalCountdown.tsx
+++ components/home/finalCountdown.tsx
@@ -1,19 +1,12 @@
 import React from "react";
 import { Countdown } from "./countdown";
 import { WaveText } from "./waveText";
-import { getAuth } from "@/db/dbreq";
-import { gate } from "@/db/permissions";
-import { getVacationText, Vakacio } from "./vakacio";
+import { getVacationText } from "./vakacio";
 
-const FinalCountdown = async ({ date }: { date: string }) => {
-  const selfUser = await getAuth();
+const FinalCountdown = ({ date }: { date: string }) => {
   return (
     <div className="mb-4 flex flex-col items-center justify-center gap-4">
-      {gate(selfUser, "tester", "boolean") ? (
-        <WaveText text={getVacationText(date)} />
-      ) : (
-        <Vakacio date={date} />
-      )}
+      <WaveText text={getVacationText(date)} />
       <Countdown date={date} />
     </div>
   );

8a7420ad48aee01527476c8a049c6788a855c9e6
Nimród Simon <snimrod28@gmail.com>
2025-06-22 22:29:05 +0200

    feat: integrate Outfit font family and create custom Text component for consistent typography

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -2,6 +2,18 @@ import { useMaterial3Theme } from '@pchmn/expo-material3-theme';
 import { useColorScheme } from 'react-native';
 import { MD3DarkTheme, MD3LightTheme, PaperProvider } from 'react-native-paper';
 import MainScreen from './MainScreen';
+import {
+  Outfit_100Thin,
+  Outfit_200ExtraLight,
+  Outfit_300Light,
+  Outfit_400Regular,
+  Outfit_500Medium,
+  Outfit_600SemiBold,
+  Outfit_700Bold,
+  Outfit_800ExtraBold,
+  Outfit_900Black,
+} from '@expo-google-fonts/outfit';
+import { useFonts } from 'expo-font';
 
 export default function App() {
   const colorScheme = useColorScheme();
@@ -14,6 +26,23 @@ export default function App() {
     colorScheme === 'dark'
       ? { ...MD3DarkTheme, colors: theme.dark }
       : { ...MD3LightTheme, colors: theme.light };
+
+  const [fontsLoaded] = useFonts({
+    Outfit_100Thin,
+    Outfit_200ExtraLight,
+    Outfit_300Light,
+    Outfit_400Regular,
+    Outfit_500Medium,
+    Outfit_600SemiBold,
+    Outfit_700Bold,
+    Outfit_800ExtraBold,
+    Outfit_900Black,
+    Outfit: Outfit_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null; // vagy egy betöltési képernyő, ha szükséges
+  }
 
   return (
     <PaperProvider theme={paperTheme}>
---
+++ packages/ui/src/android/Text.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Text as RNText, TextProps, TextStyle } from 'react-native';
+
+const fontFamilys = {
+  '100': 'Outfit_100Thin',
+  '200': 'Outfit_200ExtraLight',
+  '300': 'Outfit_300Light',
+  '400': 'Outfit_400Regular',
+  '500': 'Outfit_500Medium',
+  '600': 'Outfit_600SemiBold',
+  '700': 'Outfit_700Bold',
+  '800': 'Outfit_800ExtraBold',
+  '900': 'Outfit_900Black',
+  default: 'Outfit_400Regular', // Fallback to regular if fontWeight is not recognized
+  normal: 'Outfit_400Regular', // Normal is also regular
+  bold: 'Outfit_700Bold', // Bold is Outfit_700Bold
+  ultralight: 'Outfit_200ExtraLight', // Ultralight is Outfit_200ExtraLight
+  light: 'Outfit_300Light', // Light is Outfit_300Light
+  thin: 'Outfit_100Thin', // Thin is Outfit_100Thin
+  black: 'Outfit_900Black', // Black is Outfit_900Black
+  medium: 'Outfit_500Medium', // Medium is Outfit_500Medium
+  regular: 'Outfit_400Regular', // Regular is Outfit_400Regular
+  semibold: 'Outfit_600SemiBold', // Semibold is Outfit_600SemiBold
+  extrabold: 'Outfit_800ExtraBold', // ExtraBold is Outfit_800ExtraBold,
+  condensedBold: 'Outfit_700Bold', // CondensedBold is also Outfit_700Bold
+  condensedExtraBold: 'Outfit_800ExtraBold', // CondensedExtraBold is Outfit_800ExtraBold
+  condensed: 'Outfit_400Regular', // Condensed is also Outfit_400Regular
+  heavy: 'Outfit_900Black', // Heavy is Outfit_900Black
+};
+
+const Text: React.FC<TextProps> = (props) => {
+  const { style, ...rest } = props;
+  const fontWeight = String((style as TextStyle)?.fontWeight ?? '400'); // Default to regular if no fontWeight is provided
+  return (
+    <RNText
+      style={[
+        {
+          fontFamily: fontFamilys[fontWeight as keyof typeof fontFamilys],
+          fontWeight: undefined,
+        },
+        style,
+      ]}
+      {...rest}
+    />
+  );
+};
+
+export default Text;

c671664311fdefdb4ccc22787b96621a67af1965
Nimród Simon <snimrod28@gmail.com>
2025-09-01 21:26:17 +0200

    Create not-found page (again)

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import Layout from "./(e5vosdo)/layout";
+import NotFound from "./(e5vosdo)/not-found";
+
+export default function NotFoundPage() {
+  return (
+    <Layout>
+      <NotFound />
+    </Layout>
+  );
+}

1792c801c33531559ebc5954160e1044d399e981
Nimród Simon <snimrod28@gmail.com>
2025-09-04 21:26:31 +0200

    Refactor ThemeSwitch component: remove unused imports and simplify theme toggle logic

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -2,13 +2,12 @@
 
 import { FC } from "react";
 import { SwitchProps } from "@heroui/switch";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {
   SunFilledIcon,
   MoonFilledIcon,
   SystemThemeIcon,
 } from "@/components/icons";
-import { loadPalette } from "./themePicker";
 
 interface ThemeSwitchProps {
   className?: string;
@@ -16,7 +15,7 @@ interface ThemeSwitchProps {
 }
 
 const ThemeSwitch: FC<ThemeSwitchProps> = () => {
-  const [theme, setIsDarkMode] = useState(() => {
+  const [theme, setTheme] = useState(() => {
     if (typeof window !== "undefined" && window.localStorage) {
       return localStorage.getItem("theme") || "system";
     } else {
@@ -24,57 +23,21 @@ const ThemeSwitch: FC<ThemeSwitchProps> = () => {
     }
   });
 
-  useEffect(() => {
-    if (theme === "dark") {
-      document.documentElement.classList.remove("light");
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else if (theme === "light") {
-      document.documentElement.classList.remove("dark");
-      document.documentElement.classList.add("light");
-      localStorage.setItem("theme", "light");
-    } else {
-      localStorage.setItem("theme", "system");
-      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        document.documentElement.classList.remove("light");
-        document.documentElement.classList.add("dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-        document.documentElement.classList.add("light");
-      }
-    }
-
-    ["primary", "secondary"].forEach((selfColor) => {
-      loadPalette(
-        selfColor,
-        ["dark", "light"].includes(theme)
-          ? (theme as "dark" | "light")
-          : window.matchMedia("(prefers-color-scheme: dark)").matches
-            ? "dark"
-            : "light",
-      );
-    });
-  }, [theme]);
-
   const toggleTheme = () => {
-    setIsDarkMode((prevMode: string) =>
-      prevMode === "light" ? "dark" : prevMode === "dark" ? "system" : "light",
-    );
+    if (theme === "light") setTheme("dark");
+    else if (theme === "dark") setTheme("system");
+    else setTheme("light");
   };
 
   return (
-    <div
+    <button
       onClick={toggleTheme}
       className="fill-foreground-500 transition-all duration-300 ease-in-out hover:fill-foreground-600"
     >
-      {theme === "dark" ? (
-        <MoonFilledIcon size={22} />
-      ) : theme === "light" ? (
-        <SunFilledIcon size={22} />
-      ) : (
-        <SystemThemeIcon size={22} />
-      )}
-    </div>
+      {theme === "dark" && <MoonFilledIcon size={22} />}
+      {theme === "light" && <SunFilledIcon size={22} />}{" "}
+      {theme !== "dark" && theme !== "light" && <SystemThemeIcon size={22} />}
+    </button>
   );
 };
 

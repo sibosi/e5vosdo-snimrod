4fa00467e01f4d6692f8709e9b5bc8e953be04c5
Nimród Simon <snimrod28@gmail.com>
2025-09-23 20:22:43 +0200

    refactor layout components and add MainLayout with logLogin functionality

--- app/(media)/media/layout.tsx
+++ app/(media)/media/layout.tsx
@@ -1,117 +1,16 @@
-import "@/styles/globals.css";
-import { Metadata } from "next";
-import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
-import { Providers } from "@/app/providers";
-import { Navbar } from "@/components/navbar/navbar";
-import { Link } from "@heroui/react";
+import MainLayout, {
+  dynamic,
+  metadata,
+  viewport,
+} from "@/app/mainLayoutWithLogLoginProp";
+import { addLog } from "@/db/dbreq";
 import { headers } from "next/headers";
-import clsx from "clsx";
-import { PageNav } from "@/components/pagenav";
-import { auth } from "@/auth";
-import GoogleAnalytics from "@bradgarropy/next-google-analytics";
-import ServiceWorker from "@/components/PWA/serviceWorker";
-import { addLog, getAuth, updateUser, User } from "@/db/dbreq";
-import Cookie from "@/components/cookie";
-import OnCSSBug from "@/components/home/oncssbug";
-import Alerts from "@/components/home/alerts";
-import RunClientSide from "@/app/runClientSide";
-import MaintenanceGate from "@/components/home/maintenanceGate";
-import RunClientSideWrapper from "@/app/runClientSideWrapper";
 
-export const dynamic = "force-dynamic";
-
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  authors: [{ name: "Simon Nimród", url: siteConfig.links.mypage }],
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/icon/512.png",
-    apple: "/icon/1024.png",
-  },
-};
-
-export const viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-};
-
+export { dynamic, metadata, viewport };
 export default async function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  const session = await auth();
-  if (session?.user)
-    updateUser(session?.user as User, true).catch((e) => console.log(e));
-
-  const selfUser = await getAuth(session?.user?.email ?? undefined);
-
-  logLogin(session?.user?.email);
-
-  return (
-    <html lang="hu" suppressHydrationWarning className="bg-selfprimary-bg">
-      <head>
-        <link rel="canonical" href={siteConfig.links.home} />
-        <link rel="alternative" href={siteConfig.links.alternative} />
-        <link rel="apple-touch-icon" href="icon/1024.png" sizes="1024x1024" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="icon/1024.png" sizes="1024x1024" />
-        <link rel="icon" href="icon/512.png" sizes="512x512" />
-        <link rel="icon" href="icon/192.png" sizes="192x192" />
-        <meta name="darkreader-lock" />
-      </head>
-
-      <body
-        className={clsx(
-          "min-h-screen bg-background bg-selfprimary-bg font-sans antialiased",
-          fontSans.variable,
-          "light:bg-white",
-        )}
-        style={{
-          fontFamily: "Outfit, sans-serif",
-        }}
-      >
-        <RunClientSideWrapper />
-        <ServiceWorker />
-        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
-          <div className="relative flex h-screen flex-col bg-selfprimary-bg">
-            <Navbar selfUser={selfUser} className="bg-selfprimary-bg" />
-
-            <Cookie />
-            <Alerts />
-            <main className="flex-grow bg-selfprimary-bg pt-4">
-              <OnCSSBug />
-              <MaintenanceGate selfUser={selfUser} isActive={false}>
-                {children}
-              </MaintenanceGate>
-            </main>
-            <footer className="flex w-full items-center justify-center bg-selfprimary-bg py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 pb-14 text-current"
-                href={siteConfig.links.mypage}
-                title="Nimród oldala"
-              >
-                <span className="text-default-600">Fejlesztette</span>
-                <p className="text-selfprimary">Simon Nimród</p>
-                <span className="text-default-600">10.C</span>
-              </Link>
-              <br />
-              <PageNav />
-            </footer>
-          </div>
-        </Providers>
-        <GoogleAnalytics measurementId="G-P74RJ9THHS" />
-      </body>
-    </html>
-  );
+}: Readonly<{ children: React.ReactNode }>) {
+  return <MainLayout logLogin={logLogin}>{children}</MainLayout>;
 }
 
 async function logLogin(email: string | undefined | null) {

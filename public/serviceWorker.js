8e89c96e3491f5809707a0ac24657ef513e5c78f
Nimród Simon <snimrod28@gmail.com>
2025-09-27 23:26:32 +0200

    t1

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -27,7 +27,7 @@ self.addEventListener("push", (event) => {
     const notificationOptions = {
       body: data.body || "",
       icon: data.icon || "/favicon.ico",
-      badge: data.badge || "/icons/24-transparent-white.png",
+      // badge: data.badge || "/icons/24-transparent-white.png",
       data: data.data || {},
       tag: data.tag,
       renotify: !!data.renotify,

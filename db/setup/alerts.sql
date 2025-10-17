f80c5dc3466f662ab228e4037824e2da4b0948ae
Nimród Simon <snimrod28@gmail.com>
2025-10-18 01:11:41 +0200

    Fix: Update alert table setup to correct className values and ensure proper insertion

--- db/setup/alerts.sql
+++ db/setup/alerts.sql
@@ -1,22 +1,32 @@
 DROP TABLE IF EXISTS alerts;
-CREATE TABLE IF NOT EXISTS alerts (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    text TEXT NOT NULL,
-    className text,
-    padding BOOLEAN NOT NULL DEFAULT 0,
-    icon BOOLEAN DEFAULT 0
-);
-INSERT INTO alerts (text, className, padding, icon)
+
+CREATE TABLE
+    IF NOT EXISTS alerts (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        text TEXT NOT NULL,
+        className text,
+        padding BOOLEAN NOT NULL DEFAULT 0,
+        icon BOOLEAN DEFAULT 0
+    );
+
+INSERT INTO
+    alerts (text, className, padding, icon)
 VALUES
     (
         "<a href='https://docs.google.com/spreadsheets/d/1097V-LMPhvk4vhOe8B_zUEp2Fpf7mtGs' className='text-selfprimary-700'>Az aktuális teremcseréket itt találod.</a>",
-        "bg-selfprimary-100 border-selfprimary-300 hidden mx-5 text-md",
+        "hidden",
+        1,
+        NULL
+    ),
+    (
+        "Kedves Diákok! A jelentkezési felület holnap HiSchool program után, a délután folyamán nyílik meg.",
+        "hidden",
         1,
         NULL
     ),
     (
         "Az oldal átmenetileg karbantartás alatt áll, előfordulhat, hogy egyes funkciók nem működnek megfelelően.",
-        "bg-selfprimary-100 border-selfprimary-300 hidden mx-5 text-md",
+        "hidden",
         1,
         NULL
     );

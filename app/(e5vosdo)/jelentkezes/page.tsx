fbe43aaa5f7d5f9791889b2159d4d37231020902
Nimród Simon <81036480+sibosi@users.noreply.github.com>
2025-10-19 19:09:19 +0200

    Aktualisieren von page.tsx

--- app/(e5vosdo)/jelentkezes/page.tsx
+++ app/(e5vosdo)/jelentkezes/page.tsx
@@ -4,7 +4,6 @@ import { getAuth } from "@/db/dbreq";
 
 const SignupPage = async () => {
   const selfUser = await getAuth();
-  if (!selfUser) return <PleaseLogin />;
   return (
     <div>
       <Table selfUser={selfUser} />

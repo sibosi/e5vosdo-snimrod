f170274459c75032878c09dd66fe080fd1ed1ec5
Nimród Simon <81036480+sibosi@users.noreply.github.com>
2025-10-19 19:08:57 +0200

    Aktualisieren von layout.tsx

--- app/(e5vosdo)/jelentkezes/layout.tsx
+++ app/(e5vosdo)/jelentkezes/layout.tsx
@@ -3,9 +3,5 @@ export default function SignupLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <section className="pt-8 md:pt-10">
-      <div className="p-1 text-foreground">{children}</div>
-    </section>
-  );
+  return {children}
 }

e7a6d74fac869291ed8a472ba404cdc9ff04b5f2
Nimród Simon <81036480+sibosi@users.noreply.github.com>
2025-10-20 11:44:22 +0200

    t3

--- app/api/programok/route.ts
+++ app/api/programok/route.ts
@@ -1,15 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { resolve } from "path";
-import { promises as fs } from "fs";
 
 export async function GET(req: NextRequest) {
-  const filePath = resolve("./public/programok.pdf");
-  const fileBuffer = await fs.readFile(filePath);
-
-  return new NextResponse(fileBuffer, {
-    headers: {
-      "Content-Type": "application/pdf",
-      "Content-Disposition": 'inline; filename="sample.pdf"',
-    },
-  });
+  return NextResponse.json({ error: "Down" }, { status: 404 });
 }

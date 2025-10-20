babdfb138aca860ce6d01d1cd6624b8548eb0465
Nimród Simon <81036480+sibosi@users.noreply.github.com>
2025-10-20 14:21:37 +0200

    Revert "t4"
    This reverts commit 403a021c4043a330ae11e7ac7b67cc4a727b0a6a.

--- app/api/programok/route.ts
+++ app/api/programok/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   return NextResponse.json({ error: "Down" }, { status: 404 });
 }

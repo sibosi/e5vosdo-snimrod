899b87ac3e3717bce438b150b3a1b00e2352c396
Nimród Simon <81036480+sibosi@users.noreply.github.com>
2025-10-29 08:52:51 +0000

    refactor: Implement WelcomeTray to the main page

--- app/(e5vosdo)/page.tsx
+++ app/(e5vosdo)/page.tsx
@@ -17,7 +17,6 @@ import {
   QuickTeachers,
   QuickTeachersDev,
 } from "@/components/helyettesites/quickteacher";
-import WelcomeTray from "@/components/welcome/welcome";
 
 const PageHeadContent = async ({
   selfUser,
@@ -36,7 +35,18 @@ const PageHeadContent = async ({
       </Tray>
     );
 
-  return <WelcomeTray />;
+  return (
+    <Tray className="mx-auto max-w-md">
+      <h1 className="mb-2 text-3xl font-bold text-selfprimary-900 md:text-4xl">
+        Hiányolsz valamit? <br />
+        Netán a híreket? <br />
+        <span className="bg-gradient-to-r from-selfprimary-900 to-selfsecondary-300 bg-clip-text text-transparent">
+          Vagy az órarendedet?
+        </span>
+      </h1>
+      <LoginButton />
+    </Tray>
+  );
 };
 
 export default async function Home() {

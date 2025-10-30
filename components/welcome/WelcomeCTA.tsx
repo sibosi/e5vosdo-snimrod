a499cf17cc0464bbac4eebc4274c2101879c4b56
Nimród Simon <81036480+sibosi@users.noreply.github.com>
2025-10-30 11:54:47 +0000

    fix: adjust spacing in WelcomeCTA component for improved layout

--- components/welcome/WelcomeCTA.tsx
+++ components/welcome/WelcomeCTA.tsx
@@ -19,13 +19,13 @@ export default function WelcomeCTA() {
         <h3 className="mb-6 text-3xl font-bold text-selfprimary-900 md:text-4xl">
           Készen állsz a kezdésre?
         </h3>
-        <p className="mx-auto mb-4 max-w-2xl text-lg text-selfprimary-700">
+        <p className="mx-auto mb-7 max-w-2xl text-lg text-selfprimary-700">
           Válassz egy menő palettát és vágjunk bele!
         </p>
 
         <ThemeOptions className="grid grid-cols-3 justify-items-center gap-4 md:grid-cols-5 lg:grid-cols-9" />
 
-        <div className="mt-6 flex flex-col items-center justify-center gap-4 sm:flex-row">
+        <div className="mt-9 flex flex-col items-center justify-center gap-4 sm:flex-row">
           <Button
             size="lg"
             color="secondary"

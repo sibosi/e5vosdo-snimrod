fda46072d4b2380ab8ec43c19f7fd1872668f2d7
Nimród Simon <snimrod28@gmail.com>
2025-11-02 11:22:28 +0100

    back to schoooool

--- app/(e5vosdo)/page.tsx
+++ app/(e5vosdo)/page.tsx
@@ -58,40 +58,6 @@ export default async function Home() {
 
       <PageHeadContent selfUser={selfUser} />
 
-      <div className="my-4 space-y-2 rounded-xl bg-selfprimary-100 bg-gradient-to-r p-4 text-center text-foreground shadow-lg md:p-6">
-        <h2 className="text-xl font-bold md:text-2xl">
-          Köszönjük, hogy velünk tartottatok az Eötvös Napokon! Mindenkinek
-          kellemes őszi szünetet kívánunk!
-        </h2>
-      </div>
-
-      <div className="my-4 space-y-2 rounded-xl bg-selfprimary-100 bg-gradient-to-r p-4 text-foreground shadow-lg md:p-6">
-        <h2 className="text-xl font-bold md:text-2xl">
-          Szavazz és küldj visszajelzést!
-        </h2>
-
-        <ul className="space-y-3 font-semibold text-selfsecondary-700">
-          <a href="https://forms.gle/vzJR12HQriv5B32r5" className="block">
-            <li>⭐ Visszajelzek az egész E5N-ről! ➜</li>
-          </a>
-          <a href="https://forms.gle/85zZFTTP1aqPaaJf6" className="block">
-            <li>🎤 Visszajelzést küldök az előadásokról! ➜</li>
-          </a>
-          <a href="/osztaly-programok" className="block">
-            <li>🏫 Szavazok a legjobb osztályprogramokra a programsávból! ➜</li>
-          </a>
-        </ul>
-      </div>
-
-      <div className="responsive-video">
-        <iframe
-          title="KMT"
-          src="https://www.youtube.com/embed/BLGtv4RRVSY"
-          frameBorder="0"
-          allowFullScreen
-        ></iframe>
-      </div>
-
       {gate(selfUser, "user", "boolean") && (
         <Section
           title="Órarend"

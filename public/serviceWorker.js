c3a9c7342e8b0663af03749dafba1730b20f4cca
Nimród Simon <snimrod28@gmail.com>
2025-12-08 20:53:57 +0100

    refactor(serviceWorker): enhance cache deletion during activation

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -12,7 +12,20 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    (async () => {
+      const cacheNames = await caches.keys();
+
+      await Promise.all(
+        cacheNames.map((cacheName) => {
+          console.log(`Deleting cache: ${cacheName}`);
+          return caches.delete(cacheName);
+        }),
+      );
+
+      return self.clients.claim();
+    })(),
+  );
 });
 
 function isImageRequest(request) {

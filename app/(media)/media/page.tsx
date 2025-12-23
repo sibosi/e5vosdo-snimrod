12209dea189219a5f4891d11b8f6012327d43888
Nimród Simon <snimrod28@gmail.com>
2025-12-23 14:06:04 +0100

    feat(media): streamline unauthorized access handling by redirecting instead of rendering a message

--- app/(media)/media/page.tsx
+++ app/(media)/media/page.tsx
@@ -2,17 +2,15 @@ import { getAuth } from "@/db/dbreq";
 import PleaseLogin from "../../(e5vosdo)/me/redirectToLogin";
 import PhotoGridWrapper from "./PhotoGridWrapper";
 import { gate } from "@/db/permissions";
+import { redirect } from "next/navigation";
 
 const MediaPage = async () => {
   const selfUser = await getAuth();
   if (!selfUser) return <PleaseLogin />;
 
   if (!gate(selfUser, "media_admin", "boolean")) {
-    return (
-      <div className="p-4 text-danger-500">
-        Az oldal karbantartás alatt áll.
-      </div>
-    );
+    redirect("/media/szalagavato");
+    // return (<div className="p-4 text-danger-500">Az oldal karbantartás alatt áll.</div>);
   }
 
   return <PhotoGridWrapper />;

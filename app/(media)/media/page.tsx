6c17d4eba28115e3e58e946b8a080e4f1a68ca96
Nimród Simon <snimrod28@gmail.com>
2026-01-04 17:21:06 +0100

    refactor: simplify MediaPage component and restore link to Szalagavatós képek

--- app/(media)/media/page.tsx
+++ app/(media)/media/page.tsx
@@ -1,23 +1,23 @@
 import { getAuth } from "@/db/dbreq";
 import PleaseLogin from "../../(e5vosdo)/me/redirectToLogin";
 import PhotoGridWrapper from "./PhotoGridWrapper";
-import { gate } from "@/db/permissions";
-import { redirect } from "next/navigation";
+import { Link } from "@heroui/react";
 
 const MediaPage = async () => {
-  redirect("/media/szalagavato");
   const selfUser = await getAuth();
   if (!selfUser) return <PleaseLogin />;
 
-  if (!gate(selfUser, "media_admin", "boolean")) {
-    return (
-      <div className="p-4 text-danger-500">
-        Az oldal karbantartás alatt áll.
-      </div>
-    );
-  }
-
-  return <PhotoGridWrapper />;
+  return (
+    <div className="flex flex-col">
+      <Link
+        href="/media/szalagavato"
+        className="mx-auto w-full max-w-lg justify-center rounded-xl bg-selfsecondary-300 px-3 py-2 text-sm text-foreground"
+      >
+        <span className="text-center">Szalagavatós képek megtekintése ➜</span>
+      </Link>
+      <PhotoGridWrapper />
+    </div>
+  );
 };
 
 export default MediaPage;

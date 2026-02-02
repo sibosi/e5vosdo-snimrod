4413ff1f61469e194a091d294b02c30b45b502d7
softpocket <c0bra3055.videos@gmail.com>
2026-02-02 20:21:17 +0100

    Added ShortDay component

--- app/(e5vosdo)/page.tsx
+++ app/(e5vosdo)/page.tsx
@@ -19,6 +19,7 @@ import {
 } from "@/components/helyettesites/quickteacher";
 import { cookies, headers } from "next/headers";
 import { redirect } from "next/navigation";
+import ShortDay from "@/components/home/shortDay";
 
 export const metadata: Metadata = {
   robots: {
@@ -123,6 +124,8 @@ export default async function Home() {
           </div>
         </Section>
       )}
+
+      <ShortDay />
 
       {siteConfig.pageSections["helyettesitesek"] != "hidden" && (
         <Section

8114fbe499713de88212af757066f832c49752c7
Nimród Simon <81036480+sibosi@users.noreply.github.com>
2026-06-03 11:25:14 +0200

    Aktualisieren von page.tsx

--- app/(e5vosdo)/page.tsx
+++ app/(e5vosdo)/page.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "@/styles/bgimage.css";
 import { siteConfig } from "@/config/site";
 import HomeGeneral from "./page_general";
-import PromoAfterEvent from "../(noSidebar)/foca12h/promoAfterEvent";
 
 export const metadata: Metadata = {
   robots: {

1fb35b6e01ee112c864322e959e13d79e5e606c4
Nimród Simon <snimrod28@gmail.com>
2026-09-04 01:35:54 +0200

    Add event expo photo upload feature and integrate with existing pages

--- app/(e5vosdo)/page.tsx
+++ app/(e5vosdo)/page.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "@/styles/bgimage.css";
 import { siteConfig } from "@/config/site";
 import HomeGeneral from "./page_general";
+import MaintenanceGate from "@/components/home/maintenanceGate";
+import { getAuth } from "@/db/dbreq";
 
 export const metadata: Metadata = {
   robots: {
@@ -17,6 +19,11 @@ export const metadata: Metadata = {
   },
 };
 
-export default function Page() {
-  return <HomeGeneral />;
+export default async function Page() {
+  const selfUser = await getAuth();
+  return (
+    <MaintenanceGate selfUser={selfUser} isActive={true}>
+      <HomeGeneral />
+    </MaintenanceGate>
+  );
 }
